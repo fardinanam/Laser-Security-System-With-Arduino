2464d48c1ec924766dc82cbbce34a709966c98b3
Fardin Anam Aungon <52667920+fardinanam@users.noreply.github.com>
2022-08-20 19:28:22 +0600

    Add files via upload
    2nd ldr added and sending message implemented

--- laser_security_system_2nd_approach.ino
+++ laser_security_system_2nd_approach.ino
@@ -1,27 +1,35 @@
 #define LED 13
-#define STOPBUTTON 2
+#define STOPBUTTON 4
 #define LDRIN 3
-#define LDRIN2 4
+#define LDRIN2 2
 #define MILLISPERBIT 200
 
 #include <GPRS_Shield_Arduino.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
+//#include <GSM.h>
 
 #define PIN_TX    7
 #define PIN_RX    8
 #define BAUDRATE  9600
 #define PHONE_NUMBER  "01780212212"
+#define MESSAGE "Breach of boundary detected."
 
 GPRS gprs(PIN_TX, PIN_RX, BAUDRATE); //RX,TX,PWR,BaudRate
 
-long int lastChangeTime;
+long int lastChangeTimeOfLdr1;
+long int lastChangeTimeOfLdr2;
 bool buttonPressed;
 
-void ldrInterrupt() {
+void ldr1Interrupt() {
 //  Serial.println("ldr changed");
-  Serial.println(millis() - lastChangeTime);
-   lastChangeTime = millis(); 
+  Serial.println(millis() - lastChangeTimeOfLdr1);
+  lastChangeTimeOfLdr1 = millis(); 
+}
+
+void ldr2Interrupt() {
+  Serial.println(millis() - lastChangeTimeOfLdr2);
+  lastChangeTimeOfLdr2 = millis(); 
 }
 
 void buttonInterrupt() {
@@ -31,10 +39,12 @@ void buttonInterrupt() {
 
 void alarm() {
   Serial.println("start to call ...");
+
+  gprs.sendSMS(PHONE_NUMBER, MESSAGE);
   gprs.callUp(PHONE_NUMBER);
   
   int ledState = 1;
-  while(!buttonPressed) {
+  while(!digitalRead(STOPBUTTON)) {
     digitalWrite(LED, ledState);
     delay(500);
     ledState = !ledState;
@@ -42,7 +52,8 @@ void alarm() {
   Serial.println("alarm should turn off now");
   digitalWrite(LED, 0);
   buttonPressed = false;
-  lastChangeTime = millis();
+  lastChangeTimeOfLdr1 = millis();
+  lastChangeTimeOfLdr2 = millis();
 }
 
 void setup() {
@@ -60,20 +71,24 @@ void setup() {
   
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(LDRIN), ldrInterrupt, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(LDRIN2), ldrInterrupt, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(STOPBUTTON), buttonInterrupt,RISING);
-  lastChangeTime = millis();
+  
+  // enabling pins for interrupt
+//  pciSetup(15);
+  attachInterrupt(digitalPinToInterrupt(LDRIN), ldr1Interrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(LDRIN2), ldr2Interrupt, CHANGE);
+//  attachInterrupt(digitalPinToInterrupt(STOPBUTTON), buttonInterrupt,RISING);
+  lastChangeTimeOfLdr1 = millis();
+  lastChangeTimeOfLdr2 = millis();
   buttonPressed = false;
 }
 
 
 
 void loop() {
-  long int timeElapsed = (millis() - lastChangeTime);
-  if(timeElapsed >= 300) Serial.println("WHY----------------");
-  if(timeElapsed > (MILLISPERBIT + 100)) {
-    
+  long int timeElapsed1 = (millis() - lastChangeTimeOfLdr1);
+  long int timeElapsed2 = (millis() - lastChangeTimeOfLdr2);
+  if(timeElapsed1 >= 300 || timeElapsed2 >= 300) Serial.println("WHY----------------");
+  if(timeElapsed1 > (MILLISPERBIT + 100) || timeElapsed2 > (MILLISPERBIT + 100)) {
     alarm();
   }
 }
